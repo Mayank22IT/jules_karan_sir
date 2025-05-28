8d2cc4f34979d3f12dadc89bfa83e0ef8d078c72
Mayank Sein <161492901+Mayank22IT@users.noreply.github.com>
2025-05-28 20:49:16 +0530

    Create contactpage.tsx

---
+++ contactpage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ContactPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold">Contact Us</h1>
+      <p>Contact form or details will be here.</p>
+    </div>
+  );
+}
