2531516abd951cf26fd21099c90c04d452496675
Mayank Sein <161492901+Mayank22IT@users.noreply.github.com>
2025-05-28 20:49:43 +0530

    Create dashpage.tsx

---
+++ dashpage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold">User Dashboard</h1>
+      <p>User-specific content will be here.</p>
+    </div>
+  );
+}
